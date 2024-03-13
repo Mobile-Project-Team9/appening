cab39f332211b8290b04dc92fd4af0f712684240
Aku Nikula <aku.nikula@gmail.com>
2024-03-13 10:39:42 +0200

    More file-structure

---
+++ components/BookmarkList.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+/* List view of bookmarked events */
+/* Could include features such as 'jump to map location' */
+
+export default function BookmarkList() {
+  return (
+    <View>
+      <Text>BookmarkList</Text>
+    </View>
+  )
+}
---
+++ components/SortBy.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+/* Used for sorting events by time, location etc. */
+
+export default function SortBy() {
+  return (
+    <View>
+      <Text>SortBy</Text>
+    </View>
+  )
+}
