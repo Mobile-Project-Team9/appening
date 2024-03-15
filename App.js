aeefc725c7cd860eede7d1ebf3a0e966269a9517
Eetu Huotari <eetu142@gmail.com>
2024-03-15 12:32:42 +0200

    Trying to fetch data with npm start

--- App.js
+++ App.js
@@ -4,15 +4,16 @@ import styles from './styles/style';
 import TestVisitFinland from './data/TestData';
 import TestDataAtlas from './data/TestDataAtlas';
 
-const URL = "https://opendata.zoneatlas.com/oulu/objects.json";
+import Api from "./data/Api";
+
+// const URL = "https://opendata.zoneatlas.com/oulu/objects.json";
 
 export default function App() {
 
 
   return (
     <View style={styles.container}>
-      <TestVisitFinland />
-      <TestDataAtlas />
+      <Api/>
     </View>
   );
 }
--- data/Api.js
+++ data/Api.js
@@ -1,83 +1,90 @@
 /* API stuff here or elsewhere? */
+import { Text } from "react-native";
 
-const fetch = require("node-fetch");
-
-// This is the GraphQL query you use to fetch the data from the Datahub API.
-// You can freely add or remove any of available fields from the query to get exactly the data you need from the API.
-// This example query will only return 1 product with its id field.
-// To learn more about what data can be fetched from Datahub API see the Datahub API Instructions document and the attached GraphQL schema.
-/* const query = `query {
-  product(limit: 10) {
-    id
-  }
-}
-`; */
-
-const query = `query {
-    product(limit: 3) {
+export default function () {
+    const fetch = require("node-fetch");
+    
+    // This is the GraphQL query you use to fetch the data from the Datahub API.
+    // You can freely add or remove any of available fields from the query to get exactly the data you need from the API.
+    // This example query will only return 1 product with its id field.
+    // To learn more about what data can be fetched from Datahub API see the Datahub API Instructions document and the attached GraphQL schema.
+    /* const query = `query {
+      product(limit: 10) {
         id
-        webshopUrlPrimary
-        urlPrimary
-        type
-        duration
-        durationType
-        company {
-            businessName
-        }
-        openingHours {
-            open
-            openFrom
-            openTo
-            weekday
-        }
-        postalAddresses {
-            location
-            postalCode
-            streetName
-            city
-        }
-        productAvailableMonths {
-            month
-        }
+      }
     }
-}`;
-
-(async () => {
-    const authBody = new URLSearchParams();
-    authBody.append("grant_type", "password");
-    authBody.append("client_id", "datahub-api");
-    // This value should be the client secret you received in an email from datahub when you registered
-    authBody.append("client_secret", "ed7cd94f-727e-4cf7-879c-1c26f798bcc0");
-    // This value should be the email you used to register to Datahub as API user
-    authBody.append("username", "n2huee00@students.oamk.fi");
-    // This value should be the password for the API user
-    authBody.append("password", "MobileProject130324");
-
-    const authRes = await fetch(
-        "https://iam-datahub.visitfinland.com/auth/realms/Datahub/protocol/openid-connect/token",
-        {
-            method: "POST",
-            headers: { "Content-Type": "application/x-www-form-urlencoded" },
-            body: authBody,
+    `; */
+    
+    const query = `query {
+        product(limit: 3) {
+            id
+            webshopUrlPrimary
+            urlPrimary
+            type
+            duration
+            durationType
+            company {
+                businessName
+            }
+            openingHours {
+                open
+                openFrom
+                openTo
+                weekday
+            }
+            postalAddresses {
+                location
+                postalCode
+                streetName
+                city
+            }
+            productAvailableMonths {
+                month
+            }
         }
-    );
-
-    const accessToken = (await authRes.json()).access_token;
-
-    const res = await fetch(
-        "https://api-datahub.visitfinland.com/graphql/v1/graphql",
-        {
-            method: "POST",
-            body: JSON.stringify({
-                query,
-            }),
-            headers: {
-                "Content-Type": "application/json",
-                Authorization: `Bearer ${accessToken}`,
-            },
-        }
-    );
-
-    const queryResult = await res.json();
-    console.log(JSON.stringify(queryResult, null, 2));
-})();
+    }`;
+    
+    (async () => {
+        const authBody = new URLSearchParams();
+        authBody.append("grant_type", "password");
+        authBody.append("client_id", "datahub-api");
+        // This value should be the client secret you received in an email from datahub when you registered
+        authBody.append("client_secret", "ed7cd94f-727e-4cf7-879c-1c26f798bcc0");
+        // This value should be the email you used to register to Datahub as API user
+        authBody.append("username", "n2huee00@students.oamk.fi");
+        // This value should be the password for the API user
+        authBody.append("password", "MobileProject130324");
+    
+        const authRes = await fetch(
+            "https://iam-datahub.visitfinland.com/auth/realms/Datahub/protocol/openid-connect/token",
+            {
+                method: "POST",
+                headers: { "Content-Type": "application/x-www-form-urlencoded" },
+                body: authBody,
+            }
+        );
+    
+        const accessToken = (await authRes.json()).access_token;
+    
+        const res = await fetch(
+            "https://api-datahub.visitfinland.com/graphql/v1/graphql",
+            {
+                method: "POST",
+                body: JSON.stringify({
+                    query,
+                }),
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${accessToken}`,
+                },
+            }
+        );
+    
+        const queryResult = await res.json();
+        // console.log(JSON.stringify(queryResult, null, 2));
+    })();
+    
+        return (
+            <Text>{JSON.stringify(queryResult, null, 2)}</Text>
+        );
+    }
