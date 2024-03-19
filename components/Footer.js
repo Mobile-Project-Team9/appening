cc3f48de8a9b01b7332063718974776f8c957df4
Eetu Huotari <eetu142@gmail.com>
2024-03-19 11:18:50 +0200

    Added footer and it's styling

--- components/Footer.js
+++ components/Footer.js
@@ -1,10 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import styles from "../styles/style";
 
 export default function Footer() {
   return (
-    <View>
-      <Text>Footer</Text>
+    <View style={styles.footer}>
+      <Text style={styles.text}>Made by students for a mobile project.</Text>
+      <Text style={styles.text}>The students involved: Msaed Al-jebut, Eetu Huotari, Aku Nikula, Mariia Zhivonitko.</Text>
     </View>
   )
 }
--- styles/style.js
+++ styles/style.js
@@ -7,4 +7,21 @@ export default styles = StyleSheet.create({
       alignItems: 'center',
       justifyContent: 'center',
     },
+
+    text: {
+      fontSize: 16,
+      padding: 6,
+      color: "#fff" // You can change later
+    },
+
+    footer: {
+      flex: 1,
+      alignItems: "center",
+      justifyContent: "center",
+      backgroundColor: '#ff8090', // You can change later
+      padding: 10,
+      maxHeight: 120,
+      borderTopStartRadius: 10,
+      borderTopEndRadius: 10
+    }
   });
