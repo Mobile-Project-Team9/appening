95d40c661166f69bb198755d5556ed79a3aeb4fb
mariiazhivonitko <mariia.zhivonitko@gmail.com>
2024-03-26 13:27:56 +0200

    Add markers to map

--- screens/Home.js
+++ screens/Home.js
@@ -4,6 +4,68 @@ import MapView, { Marker } from 'react-native-maps';
 import Constants from 'expo-constants';
 import * as Location from 'expo-location';
 
+let locations = [{
+  "id": 244, "geo": {
+    "crs": {
+      "type": "name",
+      "properties": {
+        "name": "EPSG:4326"
+      }
+    },
+    "type": "Point",
+    "coordinates": [
+      65.01884726883974,
+      25.479098876314033
+    ]
+  }
+}, {
+  "id": 8, "geo": {
+    "crs": {
+      "type": "name",
+      "properties": {
+        "name": "EPSG:4326"
+      }
+    },
+    "type": "Point",
+    "coordinates": [
+      65.02117817080591,
+      25.46183242848834
+    ]
+  }
+}, {
+  "id": 9,
+  "type": "Point",
+  "geo": {
+    "crs": {
+      "type": "name",
+      "properties": {
+        "name": "EPSG:4326"
+      }
+    },
+    "type": "Point",
+    "coordinates": [
+      65.0208886133949,
+      25.46155877823356
+    ]
+  }
+}, {
+  "id": 12,
+  "type": "Point",
+  "geo": {
+    "crs": {
+      "type": "name",
+      "properties": {
+        "name": "EPSG:4326"
+      }
+    },
+    "type": "Point",
+    "coordinates": [
+      65.02090589788989,
+      25.460933442460377
+    ]
+  }
+}];
+
 const INITIAL_LATITUDE = 65.0800;
 const INITIAL_LONGITUDE = 25.4800;
 const INITIAL_LATITUDE_DELTA = 0.0922;
@@ -24,12 +86,12 @@ export default function Home() {
           return;
         }
         const location = await Location.getLastKnownPositionAsync(
-          {accuracy: Location.Accuracy.High});
+          { accuracy: Location.Accuracy.High });
         setLatitude(location.coords.latitude);
         setLongitude(location.coords.longitude);
         setIsLoading(false);
       }
-      catch(error) {
+      catch (error) {
         alert(error);
         setIsLoading(false);
       }
@@ -39,12 +101,12 @@ export default function Home() {
   if (isLoading) {
     return <View style={styles.container}>
       <Text>Retrieving location...</Text>
-      </View>
+    </View>
   }
   else {
     return (
       <View style={styles.container}>
-        <MapView 
+        <MapView
           style={styles.map}
           initialRegion={{
             latitude: INITIAL_LATITUDE,
@@ -52,13 +114,18 @@ export default function Home() {
             latitudeDelta: INITIAL_LATITUDE_DELTA,
             longitudeDelta: INITIAL_LONGITUDE_DELTA
           }}
-          // mapType="satellite"
-          >
-          <Marker
-            title="Testing"
-            coordinate={{latitude: latitude, longitude: longitude}}
-            >
-          </Marker>
+        // mapType="satellite"
+        >
+          {locations.map((location) => (
+            <Marker
+              key={location.id}
+              title={`${location.id}. location`}
+              coordinate={{
+                latitude: location.geo.coordinates[0],
+                longitude: location.geo.coordinates[1]
+              }}
+            />
+          ))}
         </MapView>
       </View>
     );
