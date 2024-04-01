caf5f5a8af3b64cb30a8eb49ed2aad46f95e5363
Msaed193 <msaed.aljebur@gmail.com>
2024-04-01 14:40:15 +0300

    ok

--- components/Auth.js
+++ components/Auth.js
@@ -5,3 +5,22 @@ signInWithEmailAndPassword, signOut } from 'firebase/auth';
 import {doc, setDoc} from 'firebase/firestore';
 import { auth, db, USERS_REF} from '../firebase/Confing';
 
+
+
+export const signUp = async(nickname, email, password) =>{
+    await createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+        setDoc(doc(db, USERS_REF, userCredential.user.uid), {
+            nickname: nickname,
+            email: userCredential.user.email
+        })
+
+
+
+    })
+    .catch((error) => {
+        console.log("Registration failed." + error.message);
+        Alert.alert("Registration failed." + error.message);
+
+    })
+}
