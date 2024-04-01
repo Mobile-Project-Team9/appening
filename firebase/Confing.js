05f24d0eb7e2b4577727b8a475552b2099ba3ce9
Msaed193 <msaed.aljebur@gmail.com>
2024-04-01 11:08:17 +0300

    ok

--- firebase/Confing.js
+++ firebase/Confing.js
@@ -1,6 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import AsyncStorge from "@react-native-async-storage/async-storage"
+import {initializeAut, getReactNativePersistence} from 'firebase/auth';
+
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -15,8 +18,19 @@ const firebaseConfig = {
   appId: "1:697655711876:web:47d0e47e1429be1361cad1"
 };
 
+
 // Initialize Firebase
+
+
 const app = initializeApp(firebaseConfig);
+const auth = initializeAut(app, {
+  persistence: getReactNativePersistence
+});
+
+export {auth} ;
+
 
 export const db = getFirestore(app);
+
+export const USERS_REF ='USERS'
 export const TODOS_REF =  'todos';
