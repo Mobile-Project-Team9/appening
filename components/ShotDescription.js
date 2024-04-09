482ec0cf273e9dac355817f4febd28da49a6b332
mariiazhivonitko <mariia.zhivonitko@gmail.com>
2024-04-09 15:37:59 +0300

    created EventDetails.js for rendering full info about event

--- components/ShotDescription.js
+++ components/ShotDescription.js
@@ -1,56 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react'; // Import useState hook
 import { Modal, View, Text, Image, ScrollView, TouchableOpacity } from 'react-native';
 import { styles } from '../styles/style';
-import { Button } from 'react-native-paper';
+import { Button, Portal } from 'react-native-paper';
 import { Ionicons } from '@expo/vector-icons';
+import EventDetails from './EventDetails';
 
 const ShotDescription = ({ visible, onRequestClose, selectedShot }) => {
     const imagePath = selectedShot?.Media?.[0]?.path;
+    const [showEventDetails, setShowEventDetails] = useState(false); // Initialize showEventDetails state
+
     return (
-        <Modal
-            visible={visible}
-            onRequestClose={onRequestClose}
-            transparent={true}
-        >
-            <ScrollView contentContainerStyle={styles.modalContainer}>
-                {selectedShot && (
-                    <View>
-                        <TouchableOpacity 
-                        onPress={onRequestClose} 
-                        style={styles.x}>
-                            <Ionicons name="close" size={24} color="black" />
-                        </TouchableOpacity>
-                        <Text style={styles.title}>{selectedShot.title}</Text>
-                        {selectedShot && imagePath && ( // Check if selectedShot and imagePath exist
-
-                            <Image
-                                source={{ uri: imagePath }}
-                                style={styles.image}
-                            />
-                        
-
-
-                        )}
-                        <Text style={styles.infoText}>Category: {selectedShot?.Categories?.[0]?.title}</Text>
-
-                        <View style= {styles.eventButton}>
-
-                        <Button
-                            title="Event page"
-                            onPress={onRequestClose}
-                            mode='contained-tonal'
-                            buttonColor='#E10069'
-                            textColor='#fff'
-                            
-                            >
-                            Event page
-                        </Button>
+        <Portal>
+            <Modal
+                visible={visible}
+                onDismiss={onRequestClose}
+                onRequestClose={onRequestClose}
+                transparent={true}
+            >
+                <ScrollView contentContainerStyle={styles.modalContainer}>
+                    {selectedShot && (
+                        <View>
+                            <TouchableOpacity
+                                onPress={onRequestClose}
+                                style={styles.x}>
+                                <Ionicons name="close" size={24} color="black" />
+                            </TouchableOpacity>
+                            <Text style={styles.title}>{selectedShot.title}</Text>
+                            {selectedShot && imagePath && (
+                                <Image
+                                    source={{ uri: imagePath }}
+                                    style={styles.image}
+                                />
+                            )}
+                            <Text style={styles.infoText}>Category: {selectedShot?.Categories?.[0]?.title}</Text>
+                            <View style={styles.eventButton}>
+                                <Button
+                                    title="Event page"
+                                    onPress={() => setShowEventDetails(true)} // Update showEventDetails state
+                                    mode='contained-tonal'
+                                    buttonColor='#E10069'
+                                    textColor='#fff'
+                                >
+                                    Event page
+                                </Button>
+                            </View>
                         </View>
-
-                    </View>
+                    )}
+                </ScrollView>
+                {showEventDetails && (
+                    <EventDetails event={selectedShot} /> // Render EventDetails component if showEventDetails is true
                 )}
-            </ScrollView>
-        </Modal>
+            </Modal>
+        </Portal>
     );
 }
 
