7a873d185f1c10fd4235c3c8720ee998ec2838ab
Msaed193 <msaed.aljebur@gmail.com>
2024-05-02 12:21:07 +0300

    ok

--- firebase/Confing.js
+++ firebase/Confing.js
@@ -1,14 +1,11 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-//import { getAuth } from "firebase/auth"; // This line is commented out and not necessary for the corrected version
-import { getFirestore } from "firebase/firestore";
+import {getFirestore} from 'firebase/firestore';
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import {initializeAuth, getReactNativePersistence} from 'firebase/auth';
+import{initializeAuth, getReactNativePersistence} from 'firebase/auth';
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+
 const firebaseConfig = {
   apiKey: "AIzaSyBNV0vmQhqCaJJVxbQ_IZ-NK7tbntE1URA",
   authDomain: "appening-12d35.firebaseapp.com",
@@ -20,13 +17,11 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
 const auth = initializeAuth(app, {
-  persistence: getReactNativePersistence(AsyncStorage)
+    persistence: getReactNativePersistence(AsyncStorage)
 });
-
-export { auth };
-
+export {auth};
 export const db = getFirestore(app);
-
-export const USERS_REF ='users';
-export const TODOS_REF = 'todos';
+export const USERS_REF = 'users';
+export const TODOS_REF = 'todos'; 
